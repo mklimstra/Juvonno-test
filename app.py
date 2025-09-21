1fe0430485866219b0932d1750fb57053aa09042
mklimstra <117306901+mklimstra@users.noreply.github.com>
2025-09-21 12:52:45 -0700

    Update app.py

--- app.py
+++ app.py
@@ -573,4 +573,4 @@ td.register_callbacks(app)
 
 # ------------------------- Main -------------------------
 if __name__ == "__main__":
-    app.run(debug=False, port=8050)
+app.run(debug=False, port=8050)
